c4315a1d03d68aef0cf8e438811514d45de68402
Alejandro <aledrums@gmail.com>
2018-10-07 00:56:37 -0400

    Added Python code to plot execution time of sort methods.

---
+++ search-sorts-time/sort_algorithms.py
@@ -0,0 +1,50 @@
+"""
+This file contains a colection of sort methods.
+
+In Python programming Language you can swap two elements
+by inverting them with the assignation operator like this:
+
+a, b = b, a
+"""
+
+def less_to(x, y):
+    return x < y
+
+def is_sorted(xs, c=less_to):
+    n = len(xs)
+
+    for i in range(0, n - 1):
+        if c(xs[i + 1], xs[i]):
+            return False
+
+    return True
+
+
+def selection_sort(xs, c=less_to):
+    n = len(xs)
+    
+    for i in range(0, n - 1):
+        m = i
+
+        for j in range(i + 1, n):
+            if c(xs[j], xs[m]):
+                m = j
+
+        xs[i], xs[m] = xs[m], xs[i]
+
+    return xs
+
+
+def insertion_sort(xs, c=less_to):
+    n = len(xs)
+
+    for i in range(1, n):
+
+        j = i
+
+        while j > 0 and c(xs[j], xs[j - 1]):
+            xs[j], xs[j - 1] = xs[j - 1], xs[j]
+            j -= 1
+
+    return xs
+
---
+++ search-sorts-time/sort_time.py
@@ -0,0 +1,70 @@
+import time
+import random
+import matplotlib.pyplot as plt
+from sort_algorithms import selection_sort
+from sort_algorithms import insertion_sort
+
+
+"""
+This file contains a program to plot the execution time of selection_sort
+and insertion_sort over unsorted and sorted arrays.
+"""
+
+
+def get_sort_time(sort_algorithm, sizes, num_rep, is_sorted=False):
+    
+    times = []
+
+    for n in sizes:
+        print('Sample size: {}\n'.format(n))
+        sample = range(n)
+
+        sum_time = 0
+        
+        for i in range(num_rep):
+            print('Iteration {}'.format(i + 1))
+
+            s = []
+            
+            for j in sample:
+                if is_sorted:
+                    s.append(j)
+                else:
+                    s.append(random.choice(sample))
+
+            t0 = time.time()
+            sort_algorithm(s)
+            t1 = time.time()
+            dt = t1 - t0
+            print('Execution time: {} seconds'.format(dt))
+            sum_time += dt
+
+        times.append((sum_time / num_rep) * 1000)
+        print('\n\n')
+
+    return times
+
+
+if __name__ == '__main__':
+    start_k = 10
+    num_dups = 7
+    num_reps = 5
+    ticks = range(start_k, start_k + num_dups)
+    sizes = map(lambda k : 2**k, ticks)
+    print('Testing selection_sort with unsorted array')
+    ss_times = get_sort_time(selection_sort, sizes, num_reps)
+    print('Testing insertion_sort with unsorted array')
+    is_times = get_sort_time(insertion_sort, sizes, num_reps)
+    print('Testing selection_sort with sorted array')
+    sss_times = get_sort_time(selection_sort, sizes, num_reps, True)
+    print('Testing insertion_sort with sorted array')
+    sis_times = get_sort_time(insertion_sort, sizes, num_reps, True)
+
+    plt.xlabel('samples size 2^k')
+    plt.ylabel('Execution time (ms)')
+    plt.xticks(sizes, ticks)
+    plt.plot(sizes, ss_times, label='selection sort to unsorted list')
+    plt.plot(sizes, is_times, label='insertion sort to unsorted list')
+    plt.plot(sizes, sss_times, label='selection sort to sorted list')
+    plt.plot(sizes, sis_times, label='insertion sort to sorted list')
+    plt.show()
